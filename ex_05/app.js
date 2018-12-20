20ceba336e9936515897d6fb0e91744457c7bc6e
Lyor <lyor.ouakil@gmail.com>
2018-12-20 14:07:38 +0100

    push exercie 5

--- ex_05/app.js
+++ ex_05/app.js
@@ -1,9 +1,12 @@
 var express = require('express')
 var app = express()
+var cookieParser = require('cookie-parser'); 
 app.set('view engine', 'ejs');
+app.use(cookieParser());
 
 app.get('/index', function (req, res) {
     res.sendFile(__dirname + '/index.html');
+    res.cookie('myFirstCooki', 'looks goods');
 })
 
 app.get('/image', function (req, res) {
@@ -19,7 +22,14 @@ app.get('/student/:x', function (req, res) {
         res.send("Error 404 : Page not found");
         return false;
     }
+    res.cookie('number', req.params.x);
+    res.cookie('name', req.query.name);
     res.render('student', {x : req.params.x, name : req.query.name}) // Send data to views ejs
+})
+
+app.get('/memory', function(req, res){
+    res.send("Student number " + req.cookies['number']  + " was here" )
+    // console.log("Cookie name : ",req.signedCookies);
 })
 
 module.exports = {
